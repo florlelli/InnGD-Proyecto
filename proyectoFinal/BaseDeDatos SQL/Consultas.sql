21cd787e791b22e6be9e828582819614356e5d9f
amayabren <brendaamaya9@gmail.com>
2024-11-01 20:48:29 -0300

    Update Consultas.sql

--- proyectoFinal/BaseDeDatos SQL/Consultas.sql
+++ proyectoFinal/BaseDeDatos SQL/Consultas.sql
@@ -1,30 +1,38 @@
-#1. Obtenemos todos los datos de productos
+#2. Lista completa de productos.
 SELECT * FROM productos;
 
-#2.Muestra los nombres de los clientes
-SELECT Nombre FROM clientes;
-
-#3. Insertar un nuevo producto
- INSERT INTO productos (idProductos, Nombre, Descripción, Marca, Submarca, Precio, Proveedor_idProveedor, Categoria_idCategoria) VALUES ("Samsung Galaxy A05s 128 GB", "NULL", "Samsung", "Samsung",300499) ;
-
-#4. Actualiza el precio de un producto
-UPDATE productos 
-SET Precio = 300000 
-WHERE idProducto = 10;
-
-#5. Elimina una dirección 
-DELETE FROM direcciones  
-WHERE idDirecciones = 2;
-
-#6 INNER JOIN
- #Muestra los productos con sus proveedores
-SELECT Productos.idProductos, Productos.Nombre AS Producto, Productos.Precio, Proveedor.Nombre AS Proveedor
-FROM Productos
-INNER JOIN Proveedor ON Productos.Proveedor_idProveedor = Proveedor.idProveedor;
-
-
-#7 INNER JOIN 
-#Muestra los clientes y sus direcciones 
+#3. Clientes y sus direcciones.
 SELECT clientes.idClientes, clientes.Nombre, clientes.Apellido, clientes.Correo, clientes.Teléfono, direcciones.Localidad, direcciones.Código_Postal, direcciones.Calle
 FROM clientes
 INNER JOIN direcciones ON clientes.idClientes = direcciones.Clientes_idClientes; 
+
+#4. Empleados activos y sus ventas.
+SELECT e.Nombre AS Empleado, COUNT(v.idVentas) AS Ventas_Realizadas
+FROM empleado e
+LEFT JOIN ventas v ON e.idEmpleado = v.Empleado_idEmpleado
+WHERE e.Activo = 1
+GROUP BY e.Nombre;
+    
+#5. Resumen de ventas por área.
+SELECT a.Nombre AS Area, COUNT(v.idVentas) AS Cantidad_Ventas, SUM(v.Total) AS Total_Ventas
+FROM ventas v
+JOIN empleado e ON v.Empleado_idEmpleado = e.idEmpleado
+JOIN area a ON e.Area_idArea = a.idArea
+GROUP BY a.Nombre;
+
+#6. Satisfacción del cliente por empleado.
+SELECT e.Nombre AS Empleado, AVG(sc.Puntos) AS Promedio_Puntos_Satisfaccion
+FROM satisfacción_cliente sc
+JOIN empleado e ON sc.Empleado_idEmpleado = e.idEmpleado
+GROUP BY e.Nombre;
+
+#7. Ventas totales por empleado.
+SELECT e.Nombre AS Empleado, SUM(v.Total) AS Total_Ventas
+FROM ventas v
+JOIN empleado e ON v.Empleado_idEmpleado = e.idEmpleado
+GROUP BY e.Nombre;
+
+#8. Productos con el stock. 
+SELECT p.Nombre AS Producto, i.Stock_actual, i.Stock_minimo
+FROM inventario i
+JOIN productos p ON i.Productos_idProductos = p.idProductos;
